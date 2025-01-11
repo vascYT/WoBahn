ad449a6f7046568979ddae942f55bf30276b705f
vasc <nico@vasc.dev>
2025-01-11 17:26:38 +0100

    add map

---
+++ src/components/Map.tsx
@@ -0,0 +1,21 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default function Map() {
+  return (
+    <MapContainer
+      className="w-screen h-screen"
+      center={[51.505, -0.09]}
+      zoom={13}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
