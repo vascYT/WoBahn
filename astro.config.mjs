d645b7825409359a8b6b599b380f7160142a9976
vasc <nico@vasc.dev>
2025-02-09 19:57:00 +0100

    bump astro & switch to tailwind vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
-
+import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [react(), tailwind()],
-
+  integrations: [react()],
   adapter: node({
     mode: "standalone",
   }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
