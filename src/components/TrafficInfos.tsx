2a8bbee99e8b2b8883a04f670d1edcf838c33bf1
vasc <nico@vasc.dev>
2025-03-05 23:04:34 +0100

    fix icon styling

--- src/components/TrafficInfos.tsx
+++ src/components/TrafficInfos.tsx
@@ -12,7 +12,7 @@ export default function TrafficInfos({ trafficInfos }: Props) {
           key={trafficInfo.name}
           className="flex items-center space-x-2 bg-orange-300 border border-orange-400 rounded-md py-3 px-2 mt-3 w-[240px] md:w-[320px] translate-x-[210px] md:translate-x-[285px] focus:translate-x-0 transition-transform"
         >
-          <AlertCircle className="stroke-orange-700 w-8" />
+          <AlertCircle className="stroke-orange-700 w-8 shrink-0" />
           <div className="text-left">
             <h1 className="font-bold">{trafficInfo.title}</h1>
             <p className="text-sm">{trafficInfo.description}</p>
